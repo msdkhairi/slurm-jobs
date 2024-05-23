83c7336f731b9cc1f3b5d0f927502d5f1d4e6c8e
Masoud KA <msdkhairi@gmail.com>
2024-05-22 22:57:47 -0700

    Initial commit

--- sj_unisam_v100.sh
+++ sj_unisam_v100.sh
@@ -2,11 +2,11 @@
 #SBATCH --account=def-ibajic
 #SBATCH --mail-user=compute@masoudka.com
 #SBATCH --mail-type=ALL
-#SBATCH --nodes=1
+#SBATCH --nodes=4
 #SBATCH --gpus-per-node=v100l:4 
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32    # There are 32 CPU cores on V100 Cedar GPU nodes
-#SBATCH --mem=0               # Request the full memory of the node
+#SBATCH --cpus-per-gpu=6    # There are 32 CPU cores on V100 Cedar GPU nodes
+#SBATCH --mem=64G               # Request the full memory of the node mem=0
 #SBATCH --time=06-12:00           # time (DD-HH:MM)
 # --------------------------
-apptainer run -C --nv -B /project/def-ibajic/mka267/workspace/unisam:/app/unisam -B /scratch/mka267/workspace-scratch/coco2017:/app/data/coco2017 -W $SLURM_TMPDIR --cwd /app/unisam/ ./py311_torch.sif python train.py
+apptainer run -C --nv -B /project/def-ibajic/mka267/workspace/unisam:/app/unisam -B /scratch/mka267/workspace-scratch/coco2017:/app/data/coco2017 -W $SLURM_TMPDIR --pwd /app/unisam/ ./py311_torch.sif python train.py
