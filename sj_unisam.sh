348032b21636080465930496daf8a59f8f119134
Masoud KA <msdkhairi@gmail.com>
2024-05-24 21:27:25 -0700

    Fix files to use multi nodes

--- sj_unisam.sh
+++ sj_unisam.sh
@@ -2,11 +2,24 @@
 #SBATCH --account=def-ibajic
 #SBATCH --mail-user=compute@masoudka.com
 #SBATCH --mail-type=ALL
-#SBATCH --nodes=1
-#SBATCH --gpus-per-node=p100l:4 
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=24    # There are 24 CPU cores on P100 Cedar GPU nodes
+#SBATCH --nodes=2
+#SBATCH --gpus-per-node=p100l:4
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=6    # There are 24 CPU cores on P100 Cedar GPU nodes
 #SBATCH --mem=0               # Request the full memory of the node
 #SBATCH --time=03-12:00           # time (DD-HH:MM)
 # --------------------------
-apptainer run -C --nv -B /project/def-ibajic/mka267/workspace/unisam:/app/unisam -B /scratch/mka267/workspace-scratch/coco2017:/app/data/coco2017 -W $SLURM_TMPDIR ./py311_torch.sif python /app/unisam/train.py
+srun apptainer run -C --nv \
+-B /project/def-ibajic/mka267/workspace/unisam:/app/unisam \
+-B /scratch/mka267/workspace-scratch/coco2017:/app/data/coco2017 \
+-W $SLURM_TMPDIR \
+--pwd /app/unisam/ \
+./py311_torch.sif \
+python train.py \
+--seed 123 \
+--data-dir "../data/coco2017" \
+--num-workers 6 \
+--num-nodes 2 \
+--batch-size 1 \
+--accumulate-grad-batches 2 \
+--lr 0.0004
--- sj_unisam_v100.sh
+++ sj_unisam_v100.sh
@@ -4,9 +4,23 @@
 #SBATCH --mail-type=ALL
 #SBATCH --nodes=4
 #SBATCH --gpus-per-node=v100l:4 
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-gpu=6    # There are 32 CPU cores on V100 Cedar GPU nodes
-#SBATCH --mem=64G               # Request the full memory of the node mem=0
+#SBATCH --ntasks=16
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=8    # There are 32 CPU cores on V100 Cedar GPU nodes
+#SBATCH --mem=160G              # Request the full memory of the node mem=0
 #SBATCH --time=06-12:00           # time (DD-HH:MM)
 # --------------------------
-apptainer run -C --nv -B /project/def-ibajic/mka267/workspace/unisam:/app/unisam -B /scratch/mka267/workspace-scratch/coco2017:/app/data/coco2017 -W $SLURM_TMPDIR --pwd /app/unisam/ ./py311_torch.sif python train.py
+apptainer run -C --nv \
+-B /project/def-ibajic/mka267/workspace/unisam:/app/unisam \
+-B /scratch/mka267/workspace-scratch/coco2017:/app/data/coco2017 \
+-W $SLURM_TMPDIR \
+--pwd /app/unisam/ \
+./py311_torch.sif \
+python train.py \
+--seed 123 \
+--data-dir "../data/coco2017" \
+--num-workers 32 \
+--num-nodes 4 \
+--batch-size 2 \
+--accumulate-grad-batches 1 \
+--lr 0.0004
