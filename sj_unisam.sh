ab92d3bcbf1ce76010df1bd4d485682bfbe39e65
Masoud Khairi Atani <msdkhairi@gmail.com>
2024-05-30 22:07:09 -0700

    chore: Update sj_unisam.sh and sj_unisam_v100.sh scripts for running UNISAM training

--- sj_unisam.sh
+++ sj_unisam.sh
@@ -5,21 +5,54 @@
 #SBATCH --nodes=2
 #SBATCH --gpus-per-node=p100l:4
 #SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=6    # There are 24 CPU cores on P100 Cedar GPU nodes
+#SBATCH --cpus-per-task=6    # 24 P100, 32 V100
 #SBATCH --mem=0               # Request the full memory of the node
-#SBATCH --time=03-12:00           # time (DD-HH:MM)
+#SBATCH --time=00-00:55           # time (DD-HH:MM)
 # --------------------------
-srun apptainer run -C --nv \
--B /project/def-ibajic/mka267/workspace/unisam:/app/unisam \
--B /scratch/mka267/workspace-scratch/coco2017:/app/data/coco2017 \
--W $SLURM_TMPDIR \
---pwd /app/unisam/ \
+
+# Print the current date and time
+current_time=$(date +"%Y-%m-%d %H:%M:%S")
+echo "Current time: $current_time"
+
+echo "nccl --version"
+nccl --version
+
+module load apptainer
+echo "apptainer version"
+apptainer version
+
+cat "$0"
+
+nvidia-smi
+
+srun mkdir -p $SLURM_TMPDIR/apptainer/workdir
+
+#export CUDA_VISIBLE_DEVICES=[0,1,2,3]
+
+PROJECT_HOME="/home/$USER/project"
+DATA_DIR="$PROJECT_HOME/data/coco2017"
+
+srun apptainer run \
+--contain \
+--nv \
+--bind /project/def-ibajic/mka267/workspace/unisam:$PROJECT_HOME/unisam \
+--bind /scratch/mka267/workspace-scratch/coco2017:$DATA_DIR \
+--workdir $SLURM_TMPDIR/apptainer/workdir \
+--pwd $PROJECT_HOME/unisam \
 ./py311_torch.sif \
 python train.py \
 --seed 123 \
---data-dir "../data/coco2017" \
---num-workers 6 \
---num-nodes 2 \
---batch-size 1 \
---accumulate-grad-batches 2 \
---lr 0.0004
+--trainer.accelerator "gpu" \
+--trainer.devices "auto" \
+--trainer.strategy "ddp" \
+--trainer.num-nodes $SLURM_JOB_NUM_NODES \
+--trainer.max-epochs 100 \
+--trainer.accumulate-grad-batches 1 \
+--datamodule.data-dir $DATA_DIR \
+--datamodule.batch-size 6 \
+--datamodule.num-workers $SLURM_CPUS_PER_TASK \
+--litmodel.learning-rate 0.0004 \
+--checkpoint-callback.dirpath "checkpoints/" \
+--checkpoint-callback.monitor "val_loss" \
+--logger.save-dir "logs/" \
+--logger.name "unisam-reconstruction" \
--- sj_unisam_v100.sh
+++ sj_unisam_v100.sh
@@ -3,24 +3,56 @@
 #SBATCH --mail-user=compute@masoudka.com
 #SBATCH --mail-type=ALL
 #SBATCH --nodes=4
-#SBATCH --gpus-per-node=v100l:4 
-#SBATCH --ntasks=16
+#SBATCH --gpus-per-node=v100l:4
 #SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=8    # There are 32 CPU cores on V100 Cedar GPU nodes
-#SBATCH --mem=160G              # Request the full memory of the node mem=0
-#SBATCH --time=06-12:00           # time (DD-HH:MM)
+#SBATCH --cpus-per-task=8    # 24 P100, 32 V100
+#SBATCH --mem=0               # Request the full memory of the node
+#SBATCH --time=06-11:55           # time (DD-HH:MM)
 # --------------------------
-apptainer run -C --nv \
--B /project/def-ibajic/mka267/workspace/unisam:/app/unisam \
--B /scratch/mka267/workspace-scratch/coco2017:/app/data/coco2017 \
--W $SLURM_TMPDIR \
---pwd /app/unisam/ \
+
+# Print the current date and time
+current_time=$(date +"%Y-%m-%d %H:%M:%S")
+echo "Current time: $current_time"
+
+echo "nccl --version"
+nccl --version
+
+module load apptainer
+echo "apptainer version"
+apptainer version
+
+cat "$0"
+
+nvidia-smi
+
+srun mkdir -p $SLURM_TMPDIR/apptainer/workdir
+
+#export CUDA_VISIBLE_DEVICES=[0,1,2,3]
+
+PROJECT_HOME="/home/$USER/project"
+DATA_DIR="$PROJECT_HOME/data/coco2017"
+
+srun apptainer run \
+--contain \
+--nv \
+--bind /project/def-ibajic/mka267/workspace/unisam:$PROJECT_HOME/unisam \
+--bind /scratch/mka267/workspace-scratch/coco2017:$DATA_DIR \
+--workdir $SLURM_TMPDIR/apptainer/workdir \
+--pwd $PROJECT_HOME/unisam \
 ./py311_torch.sif \
 python train.py \
 --seed 123 \
---data-dir "../data/coco2017" \
---num-workers 32 \
---num-nodes 4 \
---batch-size 2 \
---accumulate-grad-batches 1 \
---lr 0.0004
+--trainer.accelerator "gpu" \
+--trainer.devices "auto" \
+--trainer.strategy "ddp" \
+--trainer.num-nodes $SLURM_JOB_NUM_NODES \
+--trainer.max-epochs 100 \
+--trainer.accumulate-grad-batches 1 \
+--datamodule.data-dir $DATA_DIR \
+--datamodule.batch-size 6 \
+--datamodule.num-workers $SLURM_CPUS_PER_TASK \
+--litmodel.learning-rate 0.0004 \
+--checkpoint-callback.dirpath "checkpoints/" \
+--checkpoint-callback.monitor "val_loss" \
+--logger.save-dir "logs/" \
+--logger.name "unisam-reconstruction" \
