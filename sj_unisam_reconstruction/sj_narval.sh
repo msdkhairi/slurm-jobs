63964317ade410ba14485609db1781df512b9892
Masoud <masoud@ensc-mcl-38>
2024-06-04 11:58:53 -0700

    Update time limit in sj_narval.sh script

--- sj_unisam_reconstruction/sj_narval.sh
+++ sj_unisam_reconstruction/sj_narval.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --cpus-per-task=12    # 24 p100l, 32 v100l, 40 v100 beluga
 #SBATCH --mem=0               # Request the full memory of the node
-#SBATCH --time=00-11:55           # time (DD-HH:MM)
+#SBATCH --time=01-11:55           # time (DD-HH:MM)
 # --------------------------
 
 # Print the current date and time
@@ -29,10 +29,10 @@ srun mkdir -p $SLURM_TMPDIR/apptainer/workdir
 srun mkdir -p $SLURM_TMPDIR/data/coco2017
 #export CUDA_VISIBLE_DEVICES=[0,1,2,3]
 
-DATA_DIR="/scratch/mka267/data/coco2017"
-LOCAL_DATA_DIR="$SLURM_TMPDIR/data/"
+DATA_DIR="/scratch/mka267/data"
+LOCAL_DATA_DIR="$SLURM_TMPDIR/data"
 
-srun cp -r $DATA_DIR $LOCAL_DATA_DIR 
+#srun cp -r $DATA_DIR/coco2017 $LOCAL_DATA_DIR 
 
 current_time=$(date +"%Y-%m-%d %H:%M:%S")
 echo "Current time after cp: $current_time"
@@ -44,7 +44,7 @@ srun apptainer run \
 --contain \
 --nv \
 --bind /project/def-ibajic/mka267/workspace/UnivCollabSFU24:$PROJECT_HOME/unisam \
---bind $LOCAL_DATA_DIR/coco2017:$APPTAINER_DATA_DIR \
+--bind $DATA_DIR/coco2017:$APPTAINER_DATA_DIR \
 --workdir $SLURM_TMPDIR/apptainer/workdir \
 --pwd $PROJECT_HOME/unisam \
 ./py311_torch.sif \
