08afb7154b1c3825912000a1e4e85727644bc575
Masoud <masoud@ensc-mcl-38>
2024-06-03 19:09:47 -0700

    Update batch size, num-workers, and data directory in sj_narval.sh script

--- sj_unisam_reconstruction/sj_narval.sh
+++ sj_unisam_reconstruction/sj_narval.sh
@@ -3,11 +3,11 @@
 #SBATCH --mail-user=compute@masoudka.com
 #SBATCH --mail-type=ALL
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=v100:4
+#SBATCH --gpus-per-node=a100:4
 #SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=10    # 24 p100l, 32 v100l, 40 v100 beluga
+#SBATCH --cpus-per-task=12    # 24 p100l, 32 v100l, 40 v100 beluga
 #SBATCH --mem=0               # Request the full memory of the node
-#SBATCH --time=06-11:55           # time (DD-HH:MM)
+#SBATCH --time=00-11:55           # time (DD-HH:MM)
 # --------------------------
 
 # Print the current date and time
@@ -26,17 +26,26 @@ cat "$0"
 nvidia-smi
 
 srun mkdir -p $SLURM_TMPDIR/apptainer/workdir
+srun mkdir -p $SLURM_TMPDIR/data/coco2017
 
 #export CUDA_VISIBLE_DEVICES=[0,1,2,3]
 
+DATA_DIR="/scratch/mka267/data/coco2017"
+LOCAL_DATA_DIR="$SLURM_TMPDIR/data/"
+
+srun cp -r $DATA_DIR $LOCAL_DATA_DIR 
+
+current_time=$(date +"%Y-%m-%d %H:%M:%S")
+echo "Current time after cp: $current_time"
+
 PROJECT_HOME="/home/$USER/project"
-DATA_DIR="$PROJECT_HOME/data/coco2017"
+APPTAINER_DATA_DIR="$PROJECT_HOME/data/coco2017"
 
 srun apptainer run \
 --contain \
 --nv \
 --bind /project/def-ibajic/mka267/workspace/UnivCollabSFU24:$PROJECT_HOME/unisam \
---bind /scratch/mka267/workspace-scratch/coco2017:$DATA_DIR \
+--bind $LOCAL_DATA_DIR/coco2017:$APPTAINER_DATA_DIR \
 --workdir $SLURM_TMPDIR/apptainer/workdir \
 --pwd $PROJECT_HOME/unisam \
 ./py311_torch.sif \
@@ -48,9 +57,9 @@ python train.py \
 --trainer.num-nodes $SLURM_JOB_NUM_NODES \
 --trainer.max-epochs 100 \
 --trainer.accumulate-grad-batches 1 \
---datamodule.data-dir $DATA_DIR \
---datamodule.batch-size 3 \
---datamodule.num-workers 24 \
+--datamodule.data-dir $APPTAINER_DATA_DIR \
+--datamodule.batch-size 7 \
+--datamodule.num-workers 48 \
 --litmodel.learning-rate 0.0004 \
 --checkpoint-callback.dirpath "checkpoints_unisam_reconstruction/" \
 --checkpoint-callback.monitor "val_loss" \
