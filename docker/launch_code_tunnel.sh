e3096bed2152966f7fe608c9bb895ca6836f0282
Masoud KA <msdkhairi@gmail.com>
2024-06-13 00:35:59 +0000

    chore: Add Docker script to launch code tunnel

---
+++ docker/launch_code_tunnel.sh
@@ -0,0 +1,9 @@
+docker run --cpus=60 \
+           --memory=240g \
+           --memory-swap=240g \
+           --gpus=8 \
+           --volume /localhome/mka267/workspace:/home/workspace \
+           --volume /data/datasets:/home/data \
+           --workdir /home/workspace/ \
+           --name code-torch \
+           code-tunnel:v0.6
