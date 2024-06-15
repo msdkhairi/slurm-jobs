e8bb2b97032af6cb99a033f0d8697c8be5d03980
Masoud KA <msdkhairi@gmail.com>
2024-06-14 19:04:26 -0700

    Update launch_code_tunnel.sh

--- docker/launch_code_tunnel.sh
+++ docker/launch_code_tunnel.sh
@@ -1,6 +1,7 @@
 docker run --cpus=60 \
            --memory=240g \
            --memory-swap=240g \
+           --shm-size=120g \
            --gpus=8 \
            --volume /localhome/mka267/workspace:/home/workspace \
            --volume /data/datasets:/home/data \
